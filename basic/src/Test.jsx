6fe7fee5af3128696928d2bf4e50a1bb3157e0a1
Ealrak <wotmd0111@gmail.com>
2025-06-20 18:11:12 +0900

    리액트 컴포넌트 공부

--- basic/src/Test.jsx
+++ basic/src/Test.jsx
@@ -1,13 +1,25 @@
-import Header from "./header.jsx";
+import Header from './header.jsx';
+import './Test.css';
 //컴포넌트 생성
 function Test(){
     return(
         <>
         <Header />
+        <Aside />
         <h1>Test 컴포넌트</h1>
         </>
     )
 }
-
 //다른 컴포넌트에서 해당 Test컴포넌트 사용가능하도록 내보내기 설정
 export default Test;
+
+//자식컴포넌트
+function Aside(){
+    return(
+        <aside>
+            <h1 style={{borderBottom:'1px solid #f00'}}>서브메뉴</h1>
+            <a href="#">메뉴1</a>
+            <hr />
+        </aside>
+    )
+}
